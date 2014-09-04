717d678a21e825489ba013b666ec8c1f1b249ff1
Josh Pollock <jpollock412@gmail.com>
2014-09-04 14:50:15 -0400

    Add admin class

---
+++ includes/classes/hwp_dr_admin.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Admin page
+ *
+ * @package   hwp_dr
+ * @author    Josh Pollock <Josh@JoshPress.net>
+ * @license   GPL-2.0+
+ * @link      
+ * @copyright 2014 Josh Pollock
+ */
+
+class hwp_dr_admin {
+
+
+	/**
+	 * Holds the instance of this class.
+	 *
+	 * @since  0.0.1
+	 * @access private
+	 * @var    object
+	 */
+	private static $instance;
+
+
+	/**
+	 * Returns the instance.
+	 *
+	 * @since  0.0.1
+	 * @access public
+	 * @return object
+	 */
+	public static function init() {
+
+		if ( !self::$instance )
+			self::$instance = new hwp_dr_admin();
+
+		return self::$instance;
+	}
+} 
